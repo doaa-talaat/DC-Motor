2f36d46fb68ffb625fcb5926beb6fa37ce3a9e28
doaa-talaat <120220372+doaa-talaat@users.noreply.github.com>
2022-12-16 17:58:53 +0200

    Update DC motor.ino

--- DC motor.ino
+++ DC motor.ino
@@ -1,33 +1,21 @@
 
-int frwd (int a, int b){
+int frwd (int a, int b, int c, int d, int e, int f, int g, int h){
 digitalWrite(a,HIGH);
 digitalWrite(b,LOW);
-}
-int frwd1 (int c, int d){
 digitalWrite(c,HIGH);
 digitalWrite(d,LOW);
-}
-int frwd2 (int e, int f){
 digitalWrite(e,HIGH);
 digitalWrite(f,LOW);
-}
-int frwd3 (int g, int h){
 digitalWrite(g,HIGH);
 digitalWrite(h,LOW);
 }
-int reverse (int a, int b){
+int reverse (int a, int b, int c, int d, int e, int f, int g, int h){
 digitalWrite(a,LOW);
 digitalWrite(b,HIGH);
-}
-int reverse1 (int c, int d){
 digitalWrite(c,LOW);
 digitalWrite(d,HIGH);
-}
-int reverse2 (int e, int f){
 digitalWrite(e,LOW);
 digitalWrite(f,HIGH);
-}
-int reverse3 (int g, int h){
 digitalWrite(g,LOW);
 digitalWrite(h,HIGH);
 }
@@ -51,14 +39,10 @@ void setup() {
 }
 
 void loop() {
-frwd  (npin1,npin2); 
-frwd1 (npin3,npin4);
-frwd2 (npin5,npin6);
-frwd3 (npin7,npin8);
+frwd  (npin1,npin2,npin3,npin4,npin5,npin6,npin7,npin8); 
+
 delay(3500);
-reverse  (npin1,npin2); 
-reverse1 (npin3,npin4);
-reverse2 (npin5,npin6);
-reverse3 (npin7,npin8);
+reverse  (npin1,npin2,npin3,npin4,npin5,npin6,npin7,npin8); 
+
 delay(5000);
 }
